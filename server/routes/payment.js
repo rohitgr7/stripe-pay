8acbf36cf44775a30e8be6358d69c69ecfab662b
Rohit Gupta <30778939+rohitgr7@users.noreply.github.com>
2018-03-09 17:07:32 +0530

    Optimized async/await

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -7,17 +7,21 @@ const { requireLogin } = require('./../middlewares');
 const { charges } = stripe(stripeSecretKey);
 
 router.post('/stripe', requireLogin, async (req, res) => {
-  const { id } = req.body;
-  const charge = await charges.create({
-    amount: 500,
-    currency: 'usd',
-    source: id,
-    description: '5$ for 5 credits'
-  });
+  try {
+    const { id } = req.body;
+    const charge = await charges.create({
+      amount: 500,
+      currency: 'usd',
+      source: id,
+      description: '5$ for 5 credits'
+    });
 
-  req.user.credits += 5;
-  const user = await req.user.save();
-  res.send(user);
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  } catch (e) {
+    throw e;
+  }
 });
 
 module.exports = router;
