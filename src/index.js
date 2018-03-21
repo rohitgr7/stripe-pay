6624f4004db822b36dbcc9c87d197661b8fb66ad
Rohit Gupta <30778939+rohitgr7@users.noreply.github.com>
2018-03-21 16:06:37 +0530

    Renamed title and removed build folder

--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store';
 
 const store = configureStore();
 
-const Root = () => (
+const root = (
   <Provider store={store}>
-    <BrowserRouter>
-      <Route path="/" component={App} exact />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(root, document.getElementById('root'));
